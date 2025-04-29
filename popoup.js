b3659a11d132b5dad2b020efe73a43096a931f29
zahntheo <theozahn2021@gmail.com>
2025-04-29 19:24:29 +0200

    set attributes replaceable

--- popoup.js
+++ popoup.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.sync.get(['ratings'], function (result) {
-      const ratings = result.ratings || {};
-      const avg = calcaAvg(ratings) 
+      const ratings = result.ratings || {}; 
       // set website name
-      const url = window.location.href
-      
-      document.getElementById("website_name").textContent = 
+      const rawUrl = window.location.href
+      const domain = rawUrl.match(/^https?:\/\/(?:www\.)?([^\/?#]+)/i)[1]; 
+      document.getElementById("website_name").textContent =  domain.toString
+    
+      // set avg
+      const avg = calcaAvg(ratings) 
+      document.getElementById("user_avg_rating").textContent =  avg.toString
+
     });
     
 
